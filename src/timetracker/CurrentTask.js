1d48deee1a9109060ebb3a0ee42f9e73081dccda
Carlos Soria <carlos.soria@konfio.mx>
2017-06-01 23:59:56 -0500

    Configure Material UI

---
+++ src/timetracker/CurrentTask.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+
+export default function CurrentTask() {
+  return (
+    <RaisedButton label="Default" />
+  )
+};
+
