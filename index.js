ca5a341e628341cc4f403770983707833d15d57f
Carlos Soria <carlos.soria@konfio.mx>
2017-06-06 15:44:45 -0500

    Implement endpoint to store and retrive tasks

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+
+function withConnection(fnc, cb){
+  MongoClient.connect(process.env.MONGODB_URI, function(err, db) {
+    console.log("Connected successfully to server");
+    fnc(db, cb);
+    db.close();
+  });
+}
+
+function getTasks(db, cb){
+  var tasksCol = db.collection('tasks');
+  tasksCol.find({}).toArray(function(err, docs){
+    cb(docs);
+  });
+}
+
+function saveTasks(tasks){
+  return function storeTasks(db, cb){
+    var tasksCol = db.collection('tasks');
+    tasksCol.insertMany(tasks, function(err, result){
+      cb(result.result.ok === 1);
+    });
+  }
+}
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/build'));
 
+app.post('/tasks', function(request, response) {
+  withConnection(saveTasks(request.body), function(success){
+    response.send({success: success});
+  });
+});
+
 app.get('/tasks', function(request, response) {
-  response.send([{hola: "mundo!"}]);
+  withConnection(getTasks, function(tasks){
+    response.send(tasks);
+  });
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
